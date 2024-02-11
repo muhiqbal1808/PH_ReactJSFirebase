201dc1652b4c95df17bf44e02706591881eb7e79
Muh Iqbal <muhammadiqbal18081996@gmail.com>
2024-02-11 16:29:35 +0800

    #2

---
+++ src/config/firebase/index.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import {} from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAx9-HRWpWPIE7TgRuoLkMtCpLC4sKY44c",
+  authDomain: "simple-notes-firebase-b44a2.firebaseapp.com",
+  projectId: "simple-notes-firebase-b44a2",
+  storageBucket: "simple-notes-firebase-b44a2.appspot.com",
+  messagingSenderId: "370451762878",
+  appId: "1:370451762878:web:493172faab86f8a23898ef",
+  measurementId: "G-FV1NSKY07F",
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export default firebase;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./containers/pages/App";
+import firebase from "./config/firebase";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+console.log("Config Firebae ===>", firebase);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
